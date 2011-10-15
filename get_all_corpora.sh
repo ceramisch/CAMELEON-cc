cc93e5f4ff35c21c0d315823abc76c97c2d33c2c
Carlos Ramisch <carlosramisch@gmail.com>
2011-10-15 19:55:31 -0300

    Run the queries in each language using screen. Do not remove scripts.

--- get_all_corpora.sh
+++ get_all_corpora.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
+rm -r conf_corpus
+
 python labels/combine_labels.py conference labels/labels_frequent.en en conf_corpus 100 > get_corpus_en.sh
-sh get_corpus_en.sh
-rm get_corpus_en.sh
+screen -S en sh get_corpus_en.sh
 
 python labels/combine_labels.py conférence labels/labels_frequent.fr fr conf_corpus 100 > get_corpus_fr.sh
-sh get_corpus_fr.sh
-rm get_corpus_fr.sh
+screen -S fr sh get_corpus_fr.sh
 
 python labels/combine_labels.py conferência labels/labels_frequent.pt pt conf_corpus 100 > get_corpus_pt.sh
-sh get_corpus_pt.sh
-rm get_corpus_pt.sh
+screen -S pt sh get_corpus_pt.sh
